6899901a7b25ada4ab190ee653aa33ffec866a03
Daniel Nottingham <danielnottingham.ti@gmail.com>
2024-08-22 09:19:36 -0300

    feat: gem service_actor and operation gerenerator

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Ypfo
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_paths << "#{root}/lib/generators"
 
     # Configuration for the application, engines, and railties goes here.
     #
