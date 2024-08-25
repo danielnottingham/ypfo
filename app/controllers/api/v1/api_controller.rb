cc8f36945ca4ed1f928e0a2809a42ad9102cdce1
Daniel Nottingham <54644738+danielnottingham@users.noreply.github.com>
2024-08-25 09:14:42 -0300

    feat: pagy gem and account list operation (#7)

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -4,8 +4,13 @@ module Api
   module V1
     class ApiController < ApplicationController
       include Pundit::Authorization
+      include Pagy::Backend
 
       before_action :authenticate_devise_api_token!
+
+      def current_user
+        current_devise_api_user
+      end
     end
   end
 end
