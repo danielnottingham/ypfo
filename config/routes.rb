f621b0d052a59eb0a571f499995ea221a4879df1
Daniel Nottingham <danielnottingham.ti@gmail.com>
2024-09-13 11:43:21 -0300

    feat: rswag

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "up" => "rails/health#show", as: :rails_health_check
+
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
+
   devise_for :users
 
   namespace :api do
